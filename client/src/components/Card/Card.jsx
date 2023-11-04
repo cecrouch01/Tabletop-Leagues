a895056fecc6450ca3a27d097df9f5a26aaa71c7
Caleb Crouch <calebcrouch@Calebs-MacBook-Pro.local>
2023-11-04 16:14:29 -0400

    layout of dashboard

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,8 +1,16 @@
-const Card = () => {
-    return 
-    <>
-        <h2>Card</h2>
-    </>
+import UserCard from "../UserCard/UserCard";
+import LeagueCard from "../LeagueCard/LeagueCard";
+
+const Card = ({type}) => {
+    return (
+    <div className={`card ${type}-card`}>
+        <h2>{type} Card</h2>
+        <div>
+            {type === 'user' ? <UserCard /> : null}
+            {type === 'league' ? <LeagueCard /> : null}
+        </div>
+    </div>
+    )
 }
 
 export default Card
--- client/src/pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -1,7 +1,22 @@
+import Card from '../../components/Card/Card'
+import './Dashboard.css'
 const Dashboard = () => {
+    const recordArr = [];
+    const activeLeague = [1,2,3];
     return (
-    <>
-        <h2>Dashboard</h2>
+    <> 
+        <Card type='user'/>
+        <div className='dshbrd-contents'>
+            <section className='user-record'>
+                <h2>Record</h2>
+            </section>
+            <section className='active-leagues'>
+                <h2>Active League</h2>
+                {activeLeague.map((league, index) => {
+                    return <Card key={index} type='league' />
+                })}
+            </section>
+        </div>
     </>
     )
 }
