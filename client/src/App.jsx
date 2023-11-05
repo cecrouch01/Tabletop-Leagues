690baf910def7d88f559af2689e82c6b9071181d
Kris Kurzawa <kurzawa1@yahoo.com>
2023-11-05 15:13:16 -0500

    first draft footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,11 +8,11 @@ function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className='app'>
       <Navbar />
       <Outlet />
       <Footer />
-    </>
+    </div>
   )
 }
 
