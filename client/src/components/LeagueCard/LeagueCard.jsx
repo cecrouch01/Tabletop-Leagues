ce3b3478a48f7e9073ea9f0de33fffaf09f3c587
Caleb Crouch <calebcrouch@Calebs-MacBook-Pro.local>
2023-11-05 21:20:38 -0500

    addition of cards rough draft

--- client/src/components/LeagueCard/LeagueCard.jsx
+++ client/src/components/LeagueCard/LeagueCard.jsx
@@ -1,8 +1,22 @@
+import placeholder from '../../assets/75x75-placeholder.png' 
+import './LeagueCard.css'
 const LeagueCard = () => {
     return (
-    <>
-        <h2>League Card Info</h2>
-    </>
+    <div className='league-card-contents'>
+        <div className='league-description'>
+            <img src={placeholder} />
+            <div>
+                <h3>League Name</h3>
+                <ul>
+                    <li>Number of League Games</li>
+                    <li>Total Amount of Players</li>
+                    <li>Format</li>
+                    <li>This is just a visible note</li>
+                    <li>use useReducer instead of the card set up we have now</li>
+            </ul>
+            </div>
+        </div>
+    </div>
     )
 }
 
--- client/src/components/UserCard/UserCard.jsx
+++ client/src/components/UserCard/UserCard.jsx
@@ -1,8 +1,21 @@
+import placeholder from '../../assets/75x75-placeholder.png' 
+import './UserCard.css';
 const UserCard = () => {
     return (
-    <>
-        <h2>User Card Info</h2>
-    </>
+    <div className='user-card-contents'>
+        <div>
+            <h3>Username</h3>
+            <img src={placeholder}/>
+        </div>
+        <div className='stats-table'>
+            <h4>Stats</h4>
+            <ul className='stat-list'>
+                <li>Skill Points</li>
+                <li>Total Wins</li>
+                <li>Best Ranking</li>
+            </ul>
+        </div>
+    </div>
     )
 }
 
