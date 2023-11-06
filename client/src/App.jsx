be6a26c5f173a88e6ad1ed2e03dc350335dcbe6f
BertisJoey <joeywiesner@gmail.com>
2023-11-05 20:19:32 -0500

    added authlink to appjsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,29 @@
 // import { useState } from 'react'
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import { Outlet } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = sessionStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
 
 function App() {
   // const [count, setCount] = useState(0)
