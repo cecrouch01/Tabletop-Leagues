391e442034a4188b4feae0382774631829b856a9
Kris Kurzawa <kurzawa1@yahoo.com>
2023-11-09 20:26:51 -0500

    made dashboard match

--- client/src/pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -3,29 +3,29 @@ import LeagueCard from '../../components/LeagueCard/LeagueCard';
 import UserCard from '../../components/UserCard/UserCard';
 import './Dashboard.css'
 const Dashboard = () => {
-    const recordArr = [1,2,3];
-    const activeLeague = [1,2,3];
+    const recordArr = [1, 2, 3];
+    const activeLeague = [1, 2, 3];
     return (
-    <> 
-        <div className='dshbrd-user-card'>
-            <UserCard />
+        <div className='dshbrd-container'>
+            <div className='dshbrd-user-card'>
+                <UserCard />
+            </div>
+            <div className='dshbrd-contents'>
+                <section className='user-record'>
+                    {/* I think we can use useReducer to change the card formats instead of using different cards */}
+                    <h2 className='column-title'>Record</h2>
+                    {recordArr.map((record, index) => {
+                        return <LeagueCard key={index} />
+                    })}
+                </section>
+                <section className='active-leagues'>
+                    <h2 className='column-title'>Active Leagues</h2>
+                    {activeLeague.map((league, index) => {
+                        return <LeagueCard key={index} />
+                    })}
+                </section>
+            </div>
         </div>
-        <div className='dshbrd-contents'>
-            <section className='user-record'>
-                {/* I think we can use useReducer to change the card formats instead of using different cards */}
-                <h2>Record</h2>
-                {recordArr.map((record, index) => {
-                    return <LeagueCard key={index} />
-                })}
-            </section>
-            <section className='active-leagues'>
-                <h2>Active Leagues</h2>
-                {activeLeague.map((league, index) => {
-                    return <LeagueCard key={index} />
-                })}
-            </section>
-        </div>
-    </>
     )
 }
 
