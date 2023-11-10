80500be539c5755cc2e4c465a79ed057e8395a50
Kris Kurzawa <kurzawa1@yahoo.com>
2023-11-09 19:57:27 -0500

    matched homepage with form pages

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -3,23 +3,23 @@ import LeagueCard from "../../components/LeagueCard/LeagueCard";
 import UserCard from "../../components/UserCard/UserCard";
 import './Home.css';
 const Home = () => {
-    const highScores = [1,2,3,4,5]
-    const topLeagues = [1,2,3,4,5]
+    const highScores = [1, 2, 3, 4, 5]
+    const topLeagues = [1, 2, 3, 4, 5]
     return (
-    <div className="home-body">
-        <div>
-            <h3>Skilled Users</h3>
-            {highScores.map((user, index) => {
-                return <UserCard key={index}  />
-            })}
+        <div className="home-body">
+            <div>
+                <h2 className="column-title">Skilled Users</h2>
+                {highScores.map((user, index) => {
+                    return <UserCard key={index} />
+                })}
+            </div>
+            <div>
+                <h2 className="column-title">Top Leagues</h2>
+                {topLeagues.map((league, index) => {
+                    return <LeagueCard key={index} />
+                })}
+            </div>
         </div>
-        <div>
-         <h3>Top Leagues</h3>
-            {topLeagues.map((league, index) => {
-                return <LeagueCard key={index}/>
-            })}
-        </div>
-    </div>
     )
 };
 
