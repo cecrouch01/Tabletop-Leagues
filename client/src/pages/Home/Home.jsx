d60b72cc03e7b8a98828559c4eb4b754e1156ef5
Caleb Crouch <calebcrouch@Calebs-MacBook-Pro.local>
2023-11-10 22:09:49 -0500

    addition of Apollo sever on client side

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,12 +1,24 @@
-import Card from "../../components/Card/Card";
+import { useQuery } from "@apollo/client";
+
+
 import LeagueCard from "../../components/LeagueCard/LeagueCard";
 import UserCard from "../../components/UserCard/UserCard";
+import { QUERY_USERS, QUERY_ME } from "../../utils/queries";
 import './Home.css';
 const Home = () => {
     const highScores = [1, 2, 3, 4, 5]
     const topLeagues = [1, 2, 3, 4, 5]
+
+    
+    const { loading, data } = useQuery(QUERY_USERS);
+
+    console.log(data)
+
     return (
         <div className="home-body">
+            <button onClick={() => {
+
+            }}>test</button>
             <div>
                 <h2 className="column-title">Skilled Users</h2>
                 {highScores.map((user, index) => {
