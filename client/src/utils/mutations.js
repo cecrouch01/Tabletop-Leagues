b074c80cb12281d0e786987bf015b9740e736525
BertisJoey <joeywiesner@gmail.com>
2023-11-12 21:20:49 -0500

    mutations on front end

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -26,3 +26,49 @@ mutation AddUser($email: String!, $password: String!, $description: String, $ico
   }
 `;
 
+export const UPDATE_USER = gql`
+  mutation UpdateUser($id: String, $username: String, $email: String, $password: String, $description: $String, $icon: String) {
+    updateUser(id: $id, username: $username, email: $email, password: $password, description: $description, icon: $icon) {
+      token
+      user {
+        id
+        username
+        email
+        password
+        description
+        icon
+      }
+    }
+  }
+`;
+
+export const ADD_LEAGUE = gql`
+  mutation AddLeague($name: String, $description: String, $admin: String, $active: Boolean, $password: String) {
+    addLeague(name: $name, description: $description, admin: $admin, active: $active, password: $password) {
+      addLeague(name: $name, description: $description, admin: $admin, active: $active, password: $password)
+    } {
+      league {
+        name
+        description
+        admin
+        active
+        password
+      }
+    }
+  }
+`;
+
+export const DEACTIVATE_LEAGUE = gql`
+  mutation DeactivateLeague($leagueId: ID!, $active: boolean) {
+    deactivateLeague(leagueId: $leagueId, active: $active) {
+      league {
+        name
+        description
+        admin
+        active
+        password
+      }
+    }
+  }
+`;
+
