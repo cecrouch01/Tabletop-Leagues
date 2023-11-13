79b0d4de93c5fb651192f5c7c524f70fd36083d3
Kris Kurzawa <kurzawa1@yahoo.com>
2023-11-13 14:31:56 -0500

    fixing bugs

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -4,7 +4,7 @@ import { gql } from '@apollo/client';
 
 export const LOGIN_USER = gql`
     mutation loginUser($email: String!, $password: String!) {
-        loginUser(email: email, password: $password) {
+        loginUser(email: $email, password: $password) {
             token
             user {
                 _id
